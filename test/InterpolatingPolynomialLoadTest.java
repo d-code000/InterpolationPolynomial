6cd0b023651671a60ed623b19b5d22128be5a1e2
Disha <disha.wind@gmail.com>
2025-03-16 15:44:50 +0300

    remove bad load test

--- test/InterpolatingPolynomialLoadTest.java
+++ test/InterpolatingPolynomialLoadTest.java
@@ -67,43 +67,4 @@ class InterpolatingPolynomialLoadTest {
             );
         }
     }
-    
-    @Test
-    void removeRandomPoint() {
-        var pointsCount = 500;
-        
-        var random = new Random();
-        var points = new ArrayList<Point2D>();
-        
-        for (int i = 0; i < pointsCount; i++) {
-            points.add(new Point2D.Double(random.nextDouble(), random.nextDouble()));
-        }
-        
-        var polynomial = new InterpolatingPolynomial(points);
-
-        var startTime = System.nanoTime();
-        polynomial.removePoint(points.get(random.nextInt(pointsCount)));
-        var endTime = System.nanoTime();
-        
-        System.out.println("Время удаления: " + getDurationInSec(startTime, endTime));
-    }
-    
-    @Test
-    void testBigRandomData(){
-        var random = new Random();
-
-        var startTime = System.nanoTime();
-        for (int i = 0; i < 1000; i++) {
-            var points = new ArrayList<Point2D>();
-            for (int j = 0; j < 100; j++) {
-                points.add(new Point2D.Double(random.nextDouble(), random.nextDouble()));
-            }
-            new InterpolatingPolynomial(points);
-        }
-        var endTime = System.nanoTime();
-
-        long duration = endTime - startTime;
-        System.out.println("Время выполнения: " + duration / 1_000_000_000.0 + " с");
-    }
-
 }
